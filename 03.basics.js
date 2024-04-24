4646a45ee8d8a38b61185081962f638a6737f355
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-25 02:14:49 +0530

    Let vs var

--- 03.basics.js
+++ 03.basics.js
@@ -1,2 +1,11 @@
-let s = "Sayan Chatterjee";
-console.log(s);
+var userName = "sayan@abc";
+userName = "sayan@345";
+console.log(userName);
+
+let passWord = "sayan1234";
+ passWord = "sayan45545";
+
+ {
+    let name = "Sayan Chatterjee";
+ }
+ //console.log(name); -- This throws an error , since let is block scoped.
