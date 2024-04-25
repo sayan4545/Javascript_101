a5a652026d7ba168d8b59706b11ea0625c2adc99
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-25 18:59:41 +0530

    Data types in JS

--- 01.basics.js
+++ 01.basics.js
@@ -1,4 +1,37 @@
-console.log("Hello Javascript");//can print anything to the console
+/*
+
+it is a multi purpose and multi paradigm programming language.
+
+>console.log()--> in JS--> console is a  js object that has log function.
+
+>System.out.println()--> in Java
+
+>if you want to show user any message just write console.log(" any message");
+
+>semi colon is not mandatory in js. It increases the readabilty of the code.
+
+> console.log("Hello world"); --> shows Hello world to the user.
+
+> Different data types are required to create applications.
+
+>Types of values in JS--> 1)Undefined 2)NUll 3)Boolean 4)String 5)Number 6)Symbol  and Object--These except Object are primitive types. They are atomic 
+in nature. They don depend on other data types.
+
+>
+
+
+
+
+*/
+
+console.log("Hello JS");
+console.log("Hello World!");
+console.log("Hello Node");
+//Numbers
+console.log(12);
+console.log(3.45);
+console.log(-23);
+
 
 
 
--- 03.basics.js
+++ 03.basics.js
@@ -1,20 +1,3 @@
-var userName = "sayan@abc";
-userName = "sayan@345";
-console.log(userName);
 
-let passWord = "sayan1234";
- passWord = "sayan45545";
-
- {
-    let name = "Sayan Chatterjee";
- }
- //console.log(name); -- This throws an error , since let is block scoped.
-
- const id = 12;
- //id =10; --> const variables cannot be reassigned.
- console.log(id);
-let name = "Sayan";
-console.log(name[0])
-//Strings 
 
 
