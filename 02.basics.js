a5d5dd8651d36f7b1aa0fc992773556499e62bef
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-26 04:05:31 +0530

    Bitwise operators

--- 02.basics.js
+++ 02.basics.js
@@ -1,3 +1,59 @@
-let a = 90;
-let b =190;
-console.log(a+b);
+/*
+--3) Comparison operators
+
+-- Greater than (>)
+-- Greater than equal to (>=)
+-- leass than (<)
+-- less than equal to (<=)
+-- Below operators depend on the coercion.
+-- not equal to (!=)
+-- abstract equals to (==)
+--  strict equality(===)
+-- stricty not (!==)
+
+--4) String based operators 
+
+--  + -> Concatenate two strings and return only one. "abc"+"Sayan" -> abcSayan
+
+--5) Bitwise operators 
+These o[perators dont directly operate on numbers but instead operate on binary representaion of number.
+    -- Bitwise and ->        &
+    --Bitwise Or ->          |
+    --Bitwise Xor ->            ^
+    --Bitwise Not ->           ~
+    --Left Shift operator ->   <<
+    --Right Shift operator ->  >>
+
+    ? How these operators work?
+
+     Suppose you want to apply bit wise and operator on 5 ,3 5&3 , What it will do is
+     , it will convert the % into its binary value , 3 to its binary value. Then will apply the 
+     and operator. So binary value of 5 and 3 is 101 and 011 . So the binary value after the 
+     operation comes out as 001. The decimal value of this binary value is 1.
+
+     5|4 ?
+     binary value of 5 is 101
+     binary value of 4 is 100
+     101 || 100 ->101 
+     So the final outpuit comes out as 5.
+
+     2<<1 ?
+     convert the left operand to its binary version . =10
+     add no of zeroes to the right of the binary value same as the left operand.
+     so the binary value of the result becomes , 100
+     final result is 
+
+
+
+
+*/
+
+console.log(4>3);
+console.log(4>=3);
+console.log(4==4);
+console.log(4===4);
+
+console.log("Sayan "+"Chatterjee");
+console.log(5&3);
+console.log(5|4);
+console.log(2<<1);
