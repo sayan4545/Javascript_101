5d5480f00d22b2d99f0f0530fe6fde858d9d6034
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-25 19:06:29 +0530

    Data types in JS

--- 01.basics.js
+++ 01.basics.js
@@ -17,7 +17,12 @@ it is a multi purpose and multi paradigm programming language.
 >Types of values in JS--> 1)Undefined 2)NUll 3)Boolean 4)String 5)Number 6)Symbol  and Object--These except Object are primitive types. They are atomic 
 in nature. They don depend on other data types.
 
->
+>Strings represents texts. If we define anything in a bunch of doubkle quotes, It will be a string.
+When we print Hello JS , we are printing strings . Can be defined by single quotes and back tick also.
+
+>We dontb have a character data type in String. 'a' and "a" are both string in js.
+
+> boolean --> true or false 
 
 
 
@@ -32,6 +37,22 @@ console.log(12);
 console.log(3.45);
 console.log(-23);
 
+//Strings 
+console.log("Sayan Chatterjee");
+console.log('Sayan Chatterjee');
+
+
+//Boolean 
+console.log(true);
+console.log(false);
+
+//undefined, null
+console.log(undefined);
+console.log(null);
+
+
+//Object
+console.log({"Sayan":12});
 
 
 
